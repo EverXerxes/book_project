07e889cc6508280e8e4ee5c6801138bc97d419cb
Lou yifei <abc835352@163.com>
2018-11-26 16:27:48 +0800

    完成setting配置

--- book_project/book_project/settings.py
+++ book_project/book_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!jdt)dvsl7v2qsj3zi=00i=x%r!(if67gz@61i4p%5_fvu80#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'book_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'book_project',
+        'USER': 'root',
+        'PASSWORD':'123',
+        'PORT': 3306,
+        'HOST': '47.98.177.157'
     }
 }
 
@@ -105,18 +109,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/ShangHai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
